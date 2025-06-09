c4b1dd5f2eb176d9b474026b80ff29eee38c1ea1
suja2004 <sujan.22cs164@sode-edu.in>
2025-06-09 13:27:34 +0530

    Add FastAPI backend for vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,8 +13,7 @@ app = FastAPI()
 # CORS settings (adjust origins as needed)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
