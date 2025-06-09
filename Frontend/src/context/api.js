1e1f9aa2923baf304776190fbaebeee757db48f3
suja2004 <sujan.22cs164@sode-edu.in>
2025-06-09 13:11:07 +0530

    Add FastAPI backend for vercel

--- Frontend/src/context/api.js
+++ Frontend/src/context/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000";
+const API_URL = "https://navatar-ashen.vercel.app";
 
 export async function getBookings() {
   const response = await fetch(`${API_URL}/bookings/`);
