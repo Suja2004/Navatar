cf9d289d80aeacfe558ca99d2bede3c8923bcb4b
suja2004 <sujan.22cs164@sode-edu.in>
2025-06-09 14:58:06 +0530

    Add FastAPI backend for vercel

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./bookings.db"
+# Use your Neon PostgreSQL connection string
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://neondb_owner:npg_8HPbhwmGUN6t@ep-white-salad-a1dn62ah-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
