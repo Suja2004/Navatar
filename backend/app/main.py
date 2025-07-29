89818e6bb9fcf49c41ab5c052b3de09869a40184
suja2004 <sujan.22cs164@sode-edu.in>
2025-07-29 14:57:39 +0530

    frontend endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ origins = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:8080",
-    "https://your-frontend-app.vercel.app",
+    "https://navatar-user.vercel.app",
     "https://navatar-ashen.vercel.app",
 ]
 
